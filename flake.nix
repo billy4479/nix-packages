18ff04149790a314680306b11e393cfb55aa1299
billy4479 <giachi.ellero@gmail.com>
2025-10-10 16:08:23 +0200

    containers: init

--- flake.nix
+++ flake.nix
@@ -3,8 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-
     flake-utils.url = "github:numtide/flake-utils";
+
+    calendar-proxy = {
+      url = "github:billy4479/calendar-proxy";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
   outputs =
@@ -12,7 +19,7 @@
       nixpkgs,
       flake-utils,
       ...
-    }:
+    }@inputs:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
@@ -27,6 +34,10 @@
           apple-fonts = pkgs.callPackage ./apple-fonts { };
           google-sans = pkgs.callPackage ./google-sans { };
           qimgv-qt6 = pkgs.callPackage ./qimgv-qt6 { };
+
+          containers = {
+            calendar-proxy = inputs.calendar-proxy.packages.${system}.container;
+          };
         };
 
         devShell = pkgs.mkShell {
