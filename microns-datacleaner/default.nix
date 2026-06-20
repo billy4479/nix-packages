7183b51ff3eae59fb872f029c0ec87823bbc731f
billy4479 <giachi.ellero@gmail.com>
2026-06-20 19:12:36 +0200

    microns-datacleaner: update to microns-combiner 0.3.1

--- microns-datacleaner/default.nix
+++ microns-datacleaner/default.nix
@@ -7,24 +7,33 @@
   ...
 }:
 python3Packages.buildPythonPackage (finalAttrs: {
-  pname = "microns-datacleaner";
-  version = "0.2.1.5";
+  pname = "microns-combiner";
+  version = "0.3.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "MICrONS-Milano-CoLab";
-    repo = "MICrONS-datacleaner";
-    rev = "b2d83643f6961c021b50390a81879927a8f9720a";
-    hash = "sha256-a5LG+3yHzOXQPW/+qtEKu6lQiZonRWbEKptL0tl4+/k=";
+    repo = "MICrONS-combiner";
+    rev = "ff78bcdcf223f51deebb31c64cb64e025e0b3350";
+    hash = "sha256-mkTXN0iPFbwmAkPmp0NSSgI6YbLXjFYHOUc4u2pZyU4=";
   };
 
   build-system = with python3Packages; [
     poetry-core
   ];
 
+  nativeBuildInputs = with python3Packages; [
+    pythonRelaxDepsHook
+  ];
+
+  pythonRelaxDeps = [
+    "numpy"
+  ];
+
   dependencies = with python3Packages; [
     numpy
     standard_transform
+    matplotlib
     pandas
     caveclient
     tqdm
