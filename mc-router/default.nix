e775db1fe6828d26a9027e6b993f1234bceac9f8
billy4479 <giachi.ellero@gmail.com>
2026-06-20 19:03:23 +0200

    mc-router: update to 1.43.1

--- mc-router/default.nix
+++ mc-router/default.nix
@@ -7,16 +7,21 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "mc-router";
-  version = "1.40.1";
+  version = "1.43.1";
 
   src = fetchFromGitHub {
     owner = "itzg";
     repo = "mc-router";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-h5lIALmU8ZBXF/mJiSmm6/qUbOJF4YWkIq3hYPdZlEY=";
+    hash = "sha256-AyhAwdWsSg9jG36JHvDqDQ/2kC8NzCVIqKtjaXC9qjc=";
   };
 
-  vendorHash = "sha256-js1boC25AsHO9V9m0uei2XhW+NPPybyfNdGZ7NtAT24=";
+  postPatch = ''
+    substituteInPlace go.mod \
+      --replace-fail "go 1.26.4" "go 1.26.3"
+  '';
+
+  vendorHash = "sha256-DLVRgeNr1aYz4gJYpZNorFx/GEmtWZ11VVEZTRZwx4M=";
 
   meta = {
     description = "Routes Minecraft client connections to backend servers based upon the requested server address";
