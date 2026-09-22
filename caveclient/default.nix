4ea744c5982b1bab9846a36588483a84980d082b
billy4479 <giachi.ellero@gmail.com>
2026-09-22 12:25:14 +0000

    caveclient: update to 8.2.1
    New release requires h5py and pins pandas<3, which conflicts with
    pandas 3.x in nixpkgs, so the pandas constraint is relaxed.

--- caveclient/default.nix
+++ caveclient/default.nix
@@ -5,21 +5,30 @@
 }:
 python3Packages.buildPythonPackage (finalAttrs: {
   pname = "caveclient";
-  version = "8.1.0";
+  version = "8.2.1";
   pyproject = true;
 
   src = fetchPypi {
     inherit (finalAttrs) pname version;
-    hash = "sha256-t3mtzooUqLbKPY51ElTPw1jzHxPtvfs3NT8qx3oCYsI=";
+    hash = "sha256-146HBP75mN7CR8DZ+tuCEcTNwP71TaNfZhPuCFfemSU=";
   };
 
   build-system = with python3Packages; [
     hatchling
   ];
 
+  nativeBuildInputs = with python3Packages; [
+    pythonRelaxDepsHook
+  ];
+
+  pythonRelaxDeps = [
+    "pandas"
+  ];
+
   dependencies = with python3Packages; [
     attrs
     cachetools
+    h5py
     ipython
     jsonschema
     networkx
